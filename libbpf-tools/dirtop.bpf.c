54aab0787f8e7050d2a917e2f1656fdc9d115ee3
Pratima Bhat <Pratima.Bhat@sony.com>
2024-05-02 11:13:20 +0530

    dirtop tool

---
+++ libbpf-tools/dirtop.bpf.c
@@ -0,0 +1,101 @@
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_core_read.h>
+#include <bpf/bpf_tracing.h>
+#include "dirtop.h"
+
+#define MAX_ENTRIES 10240
+#define INODES_NUMBER 100
+#define MAX_PIDS 128
+
+const volatile bool regular_file_only = true;
+static struct file_stat zero_value = {};
+volatile const __u32 target_pid_value = 0;
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, MAX_ENTRIES);
+    __type(key, struct file_id);
+    __type(value, struct file_stat);
+} entries SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, INODES_NUMBER);
+    __type(key, __u64);
+    __type(value, __u8);
+} inode_filter_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, MAX_PIDS);
+    __type(key, __u32);
+    __type(value, __u8);
+} target_pids SEC(".maps");
+
+static int probe_entry(struct pt_regs *ctx, struct file *file, size_t count, enum op op)
+{
+    __u64 pid_tgid = bpf_get_current_pid_tgid();
+    __u32 pid = pid_tgid >> 32;
+    __u8 *pid_found;
+
+    if (target_pid_value != 0) {
+        pid_found = bpf_map_lookup_elem(&target_pids, &pid);
+        if (!pid_found)
+            return 0;
+    }
+
+    struct file_id key = {};
+    struct file_stat *valuep;
+    struct dentry *dentry;
+    struct inode *dinode;
+    struct inode *finode;
+    __u64 ino;
+
+    bpf_probe_read_kernel(&dentry, sizeof(dentry), &file->f_path.dentry);
+    if (!dentry)
+        return 0;
+
+    bpf_probe_read_kernel(&dentry, sizeof(dentry), &dentry->d_parent);
+    bpf_probe_read_kernel(&dinode, sizeof(dinode), &dentry->d_inode);
+    bpf_probe_read_kernel(&ino, sizeof(ino), &(dinode->i_ino));
+
+    if (bpf_map_lookup_elem(&inode_filter_map, &ino)) {
+        bpf_probe_read_kernel(&finode, sizeof(finode), &file->f_inode);
+        bpf_probe_read_kernel(&key.inode, sizeof(key.inode), &(finode->i_ino));
+
+        if (key.inode == 0)
+            return 0;
+
+        valuep = bpf_map_lookup_elem(&entries, &key);
+        if (!valuep) {
+            bpf_map_update_elem(&entries, &key, &zero_value, BPF_ANY);
+            valuep = bpf_map_lookup_elem(&entries, &key);
+            if (!valuep)
+                return 0;
+        }
+        if (op == READ) {
+            valuep->reads++;
+            valuep->read_bytes += count;
+        } else {
+            valuep->writes++;
+            valuep->write_bytes += count;
+        }
+        bpf_map_update_elem(&entries, &key, valuep, BPF_ANY);
+    }
+    return 0;
+}
+
+SEC("kprobe/vfs_read")
+int BPF_KPROBE(vfs_read_entry, struct file *file, char *buf, size_t count, loff_t *pos)
+{
+    return probe_entry(ctx, file, count, READ);
+}
+
+SEC("kprobe/vfs_write")
+int BPF_KPROBE(vfs_write_entry, struct file *file, const char *buf, size_t count, loff_t *pos)
+{
+    return probe_entry(ctx, file, count, WRITE);
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
---
+++ libbpf-tools/dirtop.h
@@ -0,0 +1,32 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __DIRTOP_H
+#define __DIRTOP_H
+
+#define PATH_MAX        4096
+#define TASK_COMM_LEN   16
+#define DIRECTORY_INODE {0}
+
+enum op {
+    READ,
+    WRITE,
+};
+
+struct file_id {
+    __u64 inode;
+};
+
+struct file_stat {
+    __u64 reads;
+    __u64 read_bytes;
+    __u64 writes;
+    __u64 write_bytes;
+    __u32 pid;
+
+    char filename[PATH_MAX];
+    char comm[TASK_COMM_LEN];
+    char type;
+};
+
+#endif /* __DIRTOP_H */
+
+
